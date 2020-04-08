c5478a471d67bb9d7aacb4c98ec835ae37cd5897
Unknown <thijskruithof@gmail.com>
2020-04-08 22:55:50 +0200

    Sketch should now always be full window size, without scrollbars.

--- docs/sketches/sketch.js
+++ docs/sketches/sketch.js
@@ -1,10 +1,21 @@
+
+
 function setup() 
 {
-	createCanvas(windowWidth, windowHeight);
+	var cnv = createCanvas(windowWidth, windowHeight);
+	cnv.style('display', 'block');
+	cnv.position(0, 0);
+
+	background(255, 0, 200);
+}
+
+function windowResized() 
+{
+	resizeCanvas(windowWidth, windowHeight);
 	background(255, 0, 200);
 }
 
 function draw() 
 {
-	
+
 }

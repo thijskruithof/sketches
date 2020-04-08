5df33e3d591f24bfba6e5d0370a985a3fe84aca3
Unknown <thijskruithof@gmail.com>
2020-04-08 23:34:15 +0200

    Added Victor 2d vector library

--- docs/sketches/sketch.js
+++ docs/sketches/sketch.js
@@ -21,31 +21,48 @@ function randRange(min, max)
 	return myrng.quick()*(max-min) + min;
 }
 
-
-
 function draw() 
 {
 	myrng = new Math.seedrandom('hello');
 
 	clear();
 
-	stroke(0,0,0);
+	var yLines = [];
+	var xLines = [];
 
 	var x = 0;
 	while (x < windowWidth)
 	{
 		var x2 = x + randRange(-10,30);
-		line(x, 0, x2, windowHeight);
-
+		yLines.push({a:new Victor(x,0), b:new Victor(x2,windowHeight)});
 		x += randRange(20,30);
 	}
-
 	var y = 0;
 	while (y < windowHeight)
 	{
 		var y2 = y + randRange(-10,30);
-		line(0, y, windowWidth, y2);
-
+		xLines.push({a:new Victor(0,y), b:new Victor(windowWidth,y2)});
 		y += randRange(20,30);
 	}	
+
+	// var gridPoints = [];	
+	// for (var ix=0; ix<xLines.length; ++ix)
+	// {
+	// 	for (var iy=0; iy<yLines.length; ++iy)
+	// 	{
+	// 		var xLine = xLines[ix];
+	// 		var yLine = yLines[iy];
+
+	// 		// Calculate intersection between the two lines
+
+
+	// 	}
+	// }
+
+
+	stroke(0,0,0);	
+	for (var i=0; i<yLines.length; ++i)
+		line(yLines[i].a.x, yLines[i].a.y, yLines[i].b.x, yLines[i].b.y);
+	for (var i=0; i<xLines.length; ++i)
+		line(xLines[i].a.x, xLines[i].a.y, xLines[i].b.x, xLines[i].b.y);
 }

ca351c1397c0d76436640c5beb5029efb27a7c6b
Unknown <thijskruithof@gmail.com>
2020-04-08 23:13:46 +0200

    Turned it into an irregular grid.

--- docs/sketches/sketch.js
+++ docs/sketches/sketch.js
@@ -1,12 +1,13 @@
 
+var myrng;
 
 function setup() 
 {
 	var cnv = createCanvas(windowWidth, windowHeight);
 	cnv.style('display', 'block');
-	cnv.position(0, 0);
+	cnv.position(0, 0);	
 
-	background(255, 0, 200);
+	background(255, 255, 255);
 }
 
 function windowResized() 
@@ -15,7 +16,36 @@ function windowResized()
 	background(255, 0, 200);
 }
 
+function randRange(min, max)
+{
+	return myrng.quick()*(max-min) + min;
+}
+
+
+
 function draw() 
 {
+	myrng = new Math.seedrandom('hello');
 
+	clear();
+
+	stroke(0,0,0);
+
+	var x = 0;
+	while (x < windowWidth)
+	{
+		var x2 = x + randRange(-10,30);
+		line(x, 0, x2, windowHeight);
+
+		x += randRange(20,30);
+	}
+
+	var y = 0;
+	while (y < windowHeight)
+	{
+		var y2 = y + randRange(-10,30);
+		line(0, y, windowWidth, y2);
+
+		y += randRange(20,30);
+	}	
 }

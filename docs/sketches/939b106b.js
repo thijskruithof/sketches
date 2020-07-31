1bcc0ab0ced22052cb5fd09b0f6982e73f395f81
Unknown <thijskruithof@gmail.com>
2020-07-31 17:27:23 +0200

    Set up a basic 2d map with scrolling and zooming. Working on getting very simple tile-based loading up and running.

--- docs/sketches/939b106b.js
+++ docs/sketches/939b106b.js
@@ -1,4 +1,171 @@
 
+class Rect
+{
+	constructor (min, max)
+	{
+		this.min = min.clone();
+		this.max = max.clone();
+	}
+
+	clone()
+	{
+		return new Rect(this.min, this.max);
+	}
+
+	get center()
+	{
+		return this.min.clone().mix(this.max, 0.5);
+	}
+
+	get size()
+	{
+		return this.max.clone().subtract(this.min);
+	}
+
+	overlaps(otherRect)
+	{
+		return !(this.max.x < otherRect.min.x ||
+			this.max.y < otherRect.min.y ||
+			this.min.x > otherRect.max.x ||
+			this.min.y > otherRect.max.y);
+	}
+}
+
+class View 
+{
+	constructor(screenRect)
+	{
+		// Screen dimensions
+		this.screenRect = screenRect.clone();
+
+		// World dimensions
+		this.worldCenter = new Victor(0, 0);	//< Which position (in world coords) is shown in the center of the screen
+		this.worldScale = 1.0; 					//< Size in pixels on screen of 1 world unit.
+	}
+
+	clone()
+	{
+		var newView = new View(this.screenRect);
+		newView.worldCenter = this.worldCenter.clone();
+		newView.worldScale = this.worldScale;
+		return newView;
+	}
+
+	fitToContent(worldRect)
+	{	
+		// Center
+		this.worldCenter = worldRect.center;
+
+		// Scale
+		var worldSize = worldRect.size;
+		var screenSize = this.screenRect.size;
+		var scale = new Victor(worldSize.x / screenSize.x, worldSize.y / screenSize.y);
+		this.worldScale = Math.max(scale.x, scale.y);
+	}
+
+	worldToScreenPos(worldPos)
+	{	
+		return worldPos.clone().subtract(this.worldCenter).divide(new Victor(this.worldScale, this.worldScale)).add(this.screenRect.center);
+	}
+
+	worldToScreenRect(worldRect)
+	{
+		return new Rect(this.worldToScreenPos(worldRect.min), this.worldToScreenPos(worldRect.max));
+	}
+
+	screenToWorldPos(screenPos)
+	{
+		return screenPos.clone().subtract(this.screenRect.center).multiply(new Victor(this.worldScale, this.worldScale)).add(this.worldCenter);
+	}
+}
+
+
+
+class Tile
+{
+	constructor(parent, lod, worldRect)
+	{
+		this.parent = parent;
+		this.children = []; 
+		this.lod = lod; //< 0 for highest detail
+		this.worldRect = worldRect.clone();
+		this.isLoaded = false;		
+	}
+}
+
+
+
+var gView;
+var gRootTile;
+
+// View adjustment
+var gIsPanning = false;
+var gPanInitialMouseWorldPos;
+var gPanInitialMouseView;
+var gIsZooming = false;
+var gZoomAmount;
+
+
+function createTileChildrenRecursive(tile)
+{
+	if (tile.lod == 0)
+		return;
+
+	var childTileSize = tile.worldRect.size.multiply(new Victor(0.5, 0.5));
+
+	for (var y=0; y<2; ++y)
+	{
+		for (var x=0; x<2; ++x)
+		{
+			var rectMin = tile.worldRect.min.clone().add(childTileSize.clone().multiply(new Victor(x,y)));
+			var rect = new Rect(rectMin, rectMin.clone().add(childTileSize));
+			var newTile = new Tile(tile, tile.lod-1, rect);
+			tile.children.push(newTile);
+
+			createTileChildrenRecursive(newTile);
+		}
+	}
+}
+
+
+var timeUntilNextLoad = 0;
+
+function updateFakeTileLoading()
+{
+	timeUntilNextLoad -= gRenderDeltaTime;
+	if (timeUntilNextLoad > 0)
+		return;
+	
+	timeUntilNextLoad += 0.1;
+	
+	var tile = findNotLoadedTileWithLowestLodRecursive(gRootTile,-1);
+	if (tile != null)
+	{
+		tile.isLoaded = true;
+	}
+}
+
+
+function findNotLoadedTileWithLowestLodRecursive(tile)
+{
+	var bestTile = null;
+
+	for (var i=0; i<tile.children.length; ++i)
+	{
+		var childBestTile = findNotLoadedTileWithLowestLodRecursive(tile.children[i]);
+		if (childBestTile != null)
+		{
+			if (bestTile == null || childBestTile.lod > bestTile.lod)
+				bestTile = childBestTile;
+		}
+	}
+
+	if (!tile.isLoaded && (bestTile == null || tile.lod > bestTile.lod))
+		bestTile = tile;
+
+	return bestTile;
+}
+
 
 function setup() 
 {
@@ -6,28 +173,195 @@ function setup()
 	cnv.position(0, 0);	
 	cnv.style('position', 'absolute');
 
+	// Construct our view
+	gView = new View(new Rect(new Victor(0, 0), new Victor(window.innerWidth, window.innerHeight)));
+
+	// For now just start with fitting some stuff in our view
+	gView.fitToContent(new Rect(new Victor(0,0), new Victor(16,16)));
+
+	// Create our tree of tiles
+	gRootTile = new Tile(null, 4, new Rect(new Victor(0,0), new Victor(16,16)));
+	createTileChildrenRecursive(gRootTile);
+
+	updateFakeTileLoading();
+
+	// load image
+	testImg = loadImage("sketches/939b106b/3_0_1.jpg");
+
 	// Disable any touch controls
 	cnv.style('touch-action', 'none');
 }
 
 
 
+function drawTilesRecursive(tile)
+{
+	// if (!tile.isLoaded)
+	// 	return;
+
+	if (tile.children.length > 0)
+	{
+		var allChildrenLoaded = true;
+		for (var i=0; i<tile.children.length; ++i)
+			if (!tile.children[i].isLoaded)
+		 		allChildrenLoaded = false;
+
+		// If all our children have been loaded
+		if (allChildrenLoaded)
+		{
+			for (var i=0; i<tile.children.length; ++i)
+				drawTilesRecursive(tile.children[i]);
+
+			// strokeWeight(4);
+			// if (tile.isLoaded)
+			// 	stroke(0,128,0);
+			// else
+			// 	stroke(128,0,0);
+			// noFill();
+
+			// var margin = new Victor((5-tile.lod)*8, (5-tile.lod)*8);
+			// var r = gView.worldToScreenRect(tile.worldRect);
+			// r.min.add(margin);
+			// r.max.subtract(margin);
+			// rect(r.min.x, r.min.y, r.max.x-r.min.x, r.max.y-r.min.y);			
+		
+			return;
+		}
+	}
+
+	if (!tile.isLoaded)
+		return;
+
+	var r = gView.worldToScreenRect(tile.worldRect);
+
+	strokeWeight(1);
+	stroke(0,0,0);
+	if (tile.isLoaded)
+		fill(0,255,0);
+	else
+		fill(255,0,0);
+
+	rect(r.min.x, r.min.y, r.max.x-r.min.x, r.max.y-r.min.y);
+	line(r.min.x, r.min.y, r.max.x, r.max.y);
+	line(r.min.x, r.max.y, r.max.x, r.min.y);
+}
+
+
+function drawLod0State(tile)
+{
+	if (tile.children.length > 0)
+	{
+		for (var i=0; i<tile.children.length; ++i)
+			drawLod0State(tile.children[i]);	
+		return;
+	}
+
+	if (!tile.isLoaded)
+		return;
+
+	var r = gView.worldToScreenRect(tile.worldRect);
+
+	stroke(0,0,0);
+	fill(0,128,0);	
+	rect(r.min.x, r.min.y, r.max.x-r.min.x, r.max.y-r.min.y);
+}
+
+
 function draw() 
 {
 	preDraw();
 
+	// Adjust our view's screenspace to our canvas (in case it resized)
+	gView.screenRect = new Rect(new Victor(0,0), new Victor(gRenderWidth, gRenderHeight));
+
+	// Apply view adjustment
+	if (gIsPanning)
+	{
+		var currentMouseWorldPos = gPanInitialMouseView.screenToWorldPos(getMousePos());
+		var deltaMouseWorldPos = currentMouseWorldPos.clone().subtract(gPanInitialMouseWorldPos);
+
+		gView.worldCenter.copy(gPanInitialMouseView.worldCenter).subtract(deltaMouseWorldPos);
+	}
+	if (gIsZooming)
+	{
+		gView.worldScale *= pow(2, -gZoomAmount);
+	}
+
 	background(255, 255, 255);
 
+	strokeWeight(1);	
+	stroke(0,0,0);
+
+	drawTilesRecursive(gRootTile);
+	drawLod0State(gRootTile);
+
+
+	//image(testImg, 100, 100);
+
+	drawUI();
 
 	postDraw();
 }
 
+
+function getMousePos()
+{
+	return new Victor(mouseX, mouseY);
+}
+
+function isMouseOverZoomButton(zoomIn)
+{
+	var buttonX = 100;
+	var buttonY = zoomIn ? (gRenderHeight-200) : (gRenderHeight-100);	
+
+	var dist = getMousePos().subtract(new Victor(buttonX, buttonY)).length();
+	return dist <= 75;
+}
+
+
+function drawUI()
+{
+	strokeWeight(1);
+	stroke(0,0,0);
+	fill(255,255, !gIsPanning && isMouseOverZoomButton(false) ? 0 : 255);
+	circle(100, gRenderHeight-100, 75);
+	fill(255,255, !gIsPanning && isMouseOverZoomButton(true) ? 0 : 255);
+	circle(100, gRenderHeight-200, 75);
+
+	fill(0,0,0);
+	noStroke();
+	textAlign(CENTER, CENTER);
+	textStyle(NORMAL);
+	textSize(50);
+	text('+', 100, gRenderHeight-200+4);
+	text('-', 100, gRenderHeight-100);
+}
+
 function mousePressed()
 {
-	//return false;
+	if (!gIsPanning && !gIsZooming)
+	{
+		if (isMouseOverZoomButton(true))
+		{
+			gIsZooming = true;
+			gZoomAmount = 0.01;
+		}
+		else if (isMouseOverZoomButton(false))
+		{
+			gIsZooming = true;
+			gZoomAmount = -0.01;
+		}
+		else 
+		{
+			gPanInitialMouseView = gView.clone();
+			gPanInitialMouseWorldPos = gView.screenToWorldPos(getMousePos());
+			gIsPanning = true;
+		}
+	}
 }
 
 function mouseReleased()
 {
-	//return false;
+	gIsPanning = false;
+	gIsZooming = false;
 }

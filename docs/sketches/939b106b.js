86e509a3e510e094dcefcb382b5143d0b836fc9c
Unknown <thijskruithof@gmail.com>
2020-08-07 17:28:41 +0200

    Added a license header to the source code.

--- docs/sketches/939b106b.js
+++ docs/sketches/939b106b.js
@@ -1,3 +1,23 @@
+/*
+	939b106b.js 
+	https://github.com/thijskruithof/sketches
+
+    Copyright (C) 2020 Thijs Kruithof
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 
 function toBin(num, size=4) { if (size < 1) size = 1; var s = "000000000000000000" + num.toString(2); return s.substr(s.length-size); }
 
